e130d6f51473649cb3e1502ecf594ce25f9b9c92
Josh Long <josh@joshlong.com>
2021-04-17 23:21:32 -0700

    k8s

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -11,10 +11,14 @@ export APP_NAME=kubernetes-controller
 export GCR_IMAGE_NAME=gcr.io/${PROJECT_ID}/${APP_NAME}
 docker rmi $(docker images -a -q)
 mvn -f "${ROOT_DIR}"/../pom.xml -DskipTests=true clean package spring-boot:build-image
+
 image_id=$(docker images -q $APP_NAME)
-docker tag "${image_id}" $IMAGE_NAME
-docker push $IMAGE_NAME
-echo "pushing ${image_id} to $IMAGE_NAME "
+
 echo "tagging ${GCR_IMAGE_NAME}"
+docker tag "${image_id}" $GCR_IMAGE_NAME
+
+echo "pushing ${image_id} to $GCR_IMAGE_NAME "
+docker push $GCR_IMAGE_NAME
+
 echo "deploying to Kubernetes"
 kubectl apply -f ./k8s/controller.yaml
