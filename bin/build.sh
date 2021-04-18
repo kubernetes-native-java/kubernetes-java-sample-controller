18a17ec030e725ef53a173db4647411b521dc243
Josh Long <josh@joshlong.com>
2021-04-17 19:51:18 -0700

    k8s

--- bin/build.sh
+++ bin/build.sh
@@ -4,5 +4,16 @@ HERE="$(dirname $0)"
 echo "current directory is $HERE "
 cd "${HERE}"/..
 pwd
-mvn io.spring.javaformat:spring-javaformat-maven-plugin:0.0.27:apply
-mvn -U -DskipTests=true clean package spring-boot:build-image
+#mvn io.spring.javaformat:spring-javaformat-maven-plugin:0.0.27:apply
+
+export PROJECT_ID=${GCLOUD_PROJECT}
+export ROOT_DIR=$(cd $(dirname $0) && pwd)
+export APP_NAME=kubernetes-controller
+export GCR_IMAGE_NAME=gcr.io/${PROJECT_ID}/${APP_NAME}
+docker rmi $(docker images -a -q )
+mvn -f "${ROOT_DIR}"/pom.xml -DskipTests=true clean package spring-boot:build-image
+image_id=$(docker images -q $APP_NAME)
+docker tag "${image_id}" $IMAGE_NAME
+docker push $IMAGE_NAME
+echo "pushing ${image_id} to $IMAGE_NAME "
+echo "tagging ${GCR_IMAGE_NAME}"
